c3eeb60ad3b7460119c8b1a4f0377c32c202ed34
M. Umar Haqqui <131287278+Umar-Haqqi@users.noreply.github.com>
2024-01-28 17:37:04 +0500

    redux store setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  console.log("env: ", import.meta.env.VITE_APPWRITE_URL);
+  // console.log("env: ", import.meta.env.VITE_APPWRITE_URL);
 
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>ddd</h2>
     </>
   )
 }
